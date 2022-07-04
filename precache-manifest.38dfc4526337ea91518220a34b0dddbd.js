c3d73e769ec1b7e656acd03f997b07a7ca6cac9c
Vaibhavi-Rajesh <vaibhavi.rajesh22@gmail.com>
2022-07-04 11:48:03 +0800

    Updates

---
+++ precache-manifest.38dfc4526337ea91518220a34b0dddbd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "0501991b293946f60472",
+    "url": "/react-3/static/css/main.f320e240.chunk.css"
+  },
+  {
+    "revision": "0501991b293946f60472",
+    "url": "/react-3/static/js/main.0501991b.chunk.js"
+  },
+  {
+    "revision": "e8ef3b89bfc35bfcb4a6",
+    "url": "/react-3/static/css/1.c3e8447b.chunk.css"
+  },
+  {
+    "revision": "e8ef3b89bfc35bfcb4a6",
+    "url": "/react-3/static/js/1.e8ef3b89.chunk.js"
+  },
+  {
+    "revision": "c7e5bdf64b04c80649e4",
+    "url": "/react-3/static/js/runtime~main.c7e5bdf6.js"
+  },
+  {
+    "revision": "cc26a3e11c7899137b4e582954137f07",
+    "url": "/react-3/static/media/background.cc26a3e1.jpg"
+  },
+  {
+    "revision": "e12df8baf666dfb4b1786a1a673d40ae",
+    "url": "/react-3/static/media/class cas1.e12df8ba.jpeg"
+  },
+  {
+    "revision": "56bbdaf52927e43355f65110c9bf2993",
+    "url": "/react-3/static/media/me.56bbdaf5.jpg"
+  },
+  {
+    "revision": "120e11335acc3289e66a935f91c2db7e",
+    "url": "/react-3/static/media/planet.120e1133.svg"
+  },
+  {
+    "revision": "6d372dde94e7b94ccfc96579087213f4",
+    "url": "/react-3/static/media/planet-2.6d372dde.svg"
+  },
+  {
+    "revision": "a14c984dd9654505c61446eb761954d0",
+    "url": "/react-3/static/media/planet-3.a14c984d.svg"
+  },
+  {
+    "revision": "4bc19d953f9ae8fb763329fdfdf71dba",
+    "url": "/react-3/static/media/planet-4.4bc19d95.svg"
+  },
+  {
+    "revision": "3bb2b6b3b12974fc41f722e6aee34742",
+    "url": "/react-3/static/media/about.3bb2b6b3.jpg"
+  },
+  {
+    "revision": "70b38a42c0e6cbc2128aae6364ced135",
+    "url": "/react-3/static/media/night-sky-gif-1.70b38a42.gif"
+  },
+  {
+    "revision": "067041af9c2607b9ba10eee3a65618c4",
+    "url": "/react-3/index.html"
+  }
+];
